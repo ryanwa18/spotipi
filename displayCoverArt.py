86e768dc84c9fc0cebf0055ad275f51cba50c155
Ryan Ward <ryanwa18@vt.edu>
2020-08-25 14:32:35 -0400

    Separtate code

--- displayCoverArt.py
+++ displayCoverArt.py
@@ -1,15 +1,12 @@
-
 import sys
 import spotipy
 import spotipy.util as util
 
-import time
-import sys
-
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
-from PIL import Image
 import requests
 from io import BytesIO
+from PIL import Image
+
+from transferImage import display
 
 scope = 'user-read-currently-playing'
 
@@ -38,26 +35,8 @@ if token:
       print ("Song: ", song)
       print ("Artist: ", artist)
       print ("Image URL: ", imageURL)
+
+      display(image)
 else:
     print("Can't get token for", username)
 
-# Configuration for the matrix
-# options = RGBMatrixOptions()
-# options.rows = 32
-# options.chain_length = 1
-# options.parallel = 1
-# options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
-
-# matrix = RGBMatrix(options = options)
-
-# Make image fit our screen.
-# image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-
-# matrix.SetImage(image.convert('RGB'))
-
-# try:
-#    print("Press CTRL-C to stop.")
-#    while True:
-#       time.sleep(100)
-# except KeyboardInterrupt:
-#    sys.exit(0)
---
+++ transferImage.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import time
+import sys
+
+def display(image):
+  # Configuration for the matrix
+  options = RGBMatrixOptions()
+  options.rows = 32
+  options.chain_length = 1
+  options.parallel = 1
+  options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+  matrix = RGBMatrix(options = options)
+
+  # Make image fit our screen.
+  image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+
+  matrix.SetImage(image.convert('RGB'))
+
+  try:
+     print("Press CTRL-C to stop.")
+     while True:
+        time.sleep(100)
+  except KeyboardInterrupt:
+     sys.exit(0)
