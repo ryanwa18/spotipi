e9f64d11705ee516f018bcbba4ff2ebd1fd6063e
Ryan Ward <ryanwa18@vt.edu>
2020-08-25 21:05:21 -0400

    Add script to run every 5s

--- displayCoverArt.py
+++ displayCoverArt.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import RotatingFileHandler
 import sys
 import spotipy
 import spotipy.util as util
@@ -12,6 +14,14 @@ scope = 'user-read-currently-playing'
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
+    
+    # Configures logger for storing song data    
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='spotipy.log',level=logging.INFO)
+    logger = logging.getLogger('spotipy_logger')
+
+    # automatically deletes logs more than 2000 bytes
+    handler = RotatingFileHandler('spotipy.log', maxBytes=2000,  backupCount=3)
+    logger.addHandler(handler)
 else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
@@ -23,7 +33,7 @@ if token:
     result = sp.current_user_playing_track()
     
     if result is None:
-      print("No song is currently playing")
+      logging.warning("No song is currently playing")
     else:  
       song = result["item"]["name"]
       artist = result["item"]["album"]["artists"][0]["name"]
@@ -32,11 +42,11 @@ if token:
       response = requests.get(imageURL)
       image = Image.open(BytesIO(response.content))
 
-      print ("Song: ", song)
-      print ("Artist: ", artist)
-      print ("Image URL: ", imageURL)
+      logging.info("Song: %s ", song)
+      logging.info("Artist: %s", artist)
+      logging.info("Image URL: %s ", imageURL)
 
-      display(image)
+#      display(image)
 else:
     print("Can't get token for", username)
 
---
+++ runSpotipy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+ python displayCoverArt.py $SPOTIPY_USER
+ sleep 5
+done
