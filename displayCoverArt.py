8ec3c0a15cd9f75da15ad26265d2fb4715151c4d
Ryan Ward <ryanwa18@vt.edu>
2020-08-26 15:03:06 -0400

    Use scheduler

--- displayCoverArt.py
+++ displayCoverArt.py
@@ -1,3 +1,5 @@
+import schedule
+import time
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
@@ -10,7 +12,30 @@ from PIL import Image
 
 from transferImage import display
 
-scope = 'user-read-currently-playing'
+def getSongInfo(username):
+  scope = 'user-read-currently-playing'
+  token = util.prompt_for_user_token(username, scope)
+
+  if token:
+      sp = spotipy.Spotify(auth=token)
+      result = sp.current_user_playing_track()
+    
+      if result is None:
+        logging.warning("No song is currently playing")
+      else:  
+        song = result["item"]["name"]
+        artist = result["item"]["album"]["artists"][0]["name"]
+        imageURL = result["item"]["album"]["images"][0]["url"]
+      
+        response = requests.get(imageURL)
+        image = Image.open(BytesIO(response.content))
+
+        logging.info("Song: %s ", song)
+        logging.info("Artist: %s", artist)
+        logging.info("Image URL: %s ", imageURL)
+  else:
+      print("Can't get token for", username)
+#       display(image)
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -22,31 +47,14 @@ if len(sys.argv) > 1:
     # automatically deletes logs more than 2000 bytes
     handler = RotatingFileHandler('spotipy.log', maxBytes=2000,  backupCount=3)
     logger.addHandler(handler)
+    
+    # Creates scheduler to get song info every 10s
+    schedule.every(10).seconds.do(getSongInfo, username=username)
+    
+    while 1:
+      schedule.run_pending()
+      time.sleep(1)
 else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
-
-token = util.prompt_for_user_token(username, scope)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    result = sp.current_user_playing_track()
-    
-    if result is None:
-      logging.warning("No song is currently playing")
-    else:  
-      song = result["item"]["name"]
-      artist = result["item"]["album"]["artists"][0]["name"]
-      imageURL = result["item"]["album"]["images"][0]["url"]
-      
-      response = requests.get(imageURL)
-      image = Image.open(BytesIO(response.content))
-
-      logging.info("Song: %s ", song)
-      logging.info("Artist: %s", artist)
-      logging.info("Image URL: %s ", imageURL)
-
-#      display(image)
-else:
-    print("Can't get token for", username)
 
