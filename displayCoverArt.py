6f97198bfbd69153d6ba844c06ee39cacf6cb311
Ryan Ward <ryanwa18@vt.edu>
2020-08-26 15:14:37 -0400

    Split main code

--- displayCoverArt.py
+++ displayCoverArt.py
@@ -1,41 +1,9 @@
 import schedule
 import time
+import sys
 import logging
 from logging.handlers import RotatingFileHandler
-import sys
-import spotipy
-import spotipy.util as util
-
-import requests
-from io import BytesIO
-from PIL import Image
-
-from transferImage import display
-
-def getSongInfo(username):
-  scope = 'user-read-currently-playing'
-  token = util.prompt_for_user_token(username, scope)
-
-  if token:
-      sp = spotipy.Spotify(auth=token)
-      result = sp.current_user_playing_track()
-    
-      if result is None:
-        logging.warning("No song is currently playing")
-      else:  
-        song = result["item"]["name"]
-        artist = result["item"]["album"]["artists"][0]["name"]
-        imageURL = result["item"]["album"]["images"][0]["url"]
-      
-        response = requests.get(imageURL)
-        image = Image.open(BytesIO(response.content))
-
-        logging.info("Song: %s ", song)
-        logging.info("Artist: %s", artist)
-        logging.info("Image URL: %s ", imageURL)
-  else:
-      print("Can't get token for", username)
-#       display(image)
+from getSongInfo import getSongInfo
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
