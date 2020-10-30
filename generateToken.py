c8a19f3346504fe87843cd936ac6aa31e046e2d0
Ryan Ward <ryanwa18@vt.edu>
2020-10-30 23:24:30 +0000

    Remove python files from root

--- generateToken.py
+++
@@ -1,8 +0,0 @@
-import sys
-import spotipy
-import spotipy.util as util
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-    scope = 'user-read-currently-playing'
-    token = util.prompt_for_user_token(username, scope)
