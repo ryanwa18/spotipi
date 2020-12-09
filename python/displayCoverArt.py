8e51a353e09f06e452b5e20468d63588a24f756d
Ryan Ward <ryanwa18@vt.edu>
2020-12-09 03:47:36 +0000

    Add default image, remove scrolling

--- python/displayCoverArt.py
+++ python/displayCoverArt.py
@@ -3,10 +3,20 @@ import sys
 import logging
 from logging.handlers import RotatingFileHandler
 from getSongInfo import getSongInfo
+import requests
+from io import BytesIO
+from PIL import Image
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+import sys,os
+import configparser
 
 if len(sys.argv) > 2:
     username = sys.argv[1]
     token_path = sys.argv[2]
+
+    # Configuration file    
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, '../config/rgb_options.ini')
 
     # Configures logger for storing song data    
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='spotipy.log',level=logging.INFO)
@@ -16,8 +26,40 @@ if len(sys.argv) > 2:
     handler = RotatingFileHandler('spotipy.log', maxBytes=2000,  backupCount=3)
     logger.addHandler(handler)
 
-    while 1:
-      getSongInfo(username, token_path)
+    # Configuration for the matrix
+    config = configparser.ConfigParser()
+    config.read(filename)
+
+    options = RGBMatrixOptions()
+    options.rows = int(config['DEFAULT']['rows'])
+    options.cols = int(config['DEFAULT']['columns'])
+    options.chain_length = int(config['DEFAULT']['chain_length'])
+    options.parallel = int(config['DEFAULT']['parallel'])
+    options.hardware_mapping = config['DEFAULT']['hardware_mapping']
+    options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
+    options.brightness = int(config['DEFAULT']['brightness'])
+    
+    default_image = os.path.join(dir, config['DEFAULT']['default_image'])
+    print(default_image)
+    matrix = RGBMatrix(options = options)
+    
+    try:
+      while True:
+        try:
+          imageURL = getSongInfo(username, token_path)[1]
+          response = requests.get(imageURL)
+          image = Image.open(BytesIO(response.content))
+          image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+          matrix.SetImage(image.convert('RGB'))
+          time.sleep(1)
+        except:
+          image = Image.open(default_image)
+          image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+          matrix.SetImage(image.convert('RGB'))
+          time.sleep(1)
+    except KeyboardInterrupt:
+      sys.exit(0)
+
 else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
--- python/getSongInfo.py
+++ python/getSongInfo.py
@@ -8,6 +8,12 @@ from PIL import Image
 
 from transferImage import display
 
+#def saveImage(imageURL):
+#  response = requests.get(imageURL)
+#  image = Image.open(BytesIO(response.content))
+#  image.resize((64,64),Image.ANTIALIAS)
+#  image.save('../images/currentSong.png', optimize=True, quality=10)        
+
 def getSongInfo(username, token_path):
   scope = 'user-read-currently-playing'
   token = util.prompt_for_user_token(username, scope, cache_path=token_path)
@@ -17,20 +23,24 @@ def getSongInfo(username, token_path):
       result = sp.current_user_playing_track()
     
       if result is None:
-        logging.warning("No song is currently playing")
+ #       logging.warning("No song is currently playing")
+         print "No song playing"
       else:  
         song = result["item"]["name"]
         artist = result["item"]["album"]["artists"][0]["name"]
         imageURL = result["item"]["album"]["images"][0]["url"]
         # Remaining seconds in the song
-        timeLeft = (result["item"]["duration_ms"]/1000) - (result["progress_ms"]/1000) 
-        response = requests.get(imageURL)
-        image = Image.open(BytesIO(response.content))
-
-        logging.info("Song: %s ", song)
-        logging.info("Artist: %s", artist)
-        logging.info("Image URL: %s ", imageURL)
-        logging.info("Time Left: %ss ", timeLeft)
-        display(image, timeLeft)
+#        timeLeft = (result["item"]["duration_ms"]/1000) - (result["progress_ms"]/1000) 
+#        response = requests.get(imageURL)
+#        image = Image.open(BytesIO(response.content))
+#        image.resize((64,64),Image.ANTIALIAS)
+#        logging.info("Song: %s ", song)
+#        logging.info("Artist: %s", artist)
+#        logging.info("Image URL: %s ", imageURL)
+#        logging.info("Time Left: %ss ", timeLeft)
+#        display(image, timeLeft)
+        return [song, imageURL]
   else:
       print("Can't get token for", username)
+      return None
+  
