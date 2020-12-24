8128d0b21b3dd3cdb38bd07d203a761ec8e0261b
Ryan Ward <ryanwa18@vt.edu>
2020-12-24 16:19:58 -0500

    Finish client

--- python/client/app.py
+++ python/client/app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request
 import sys,os
 import configparser
+import dbus
 
 app = Flask(__name__)
 app.config["CACHE_TYPE"] = "null"
@@ -13,27 +14,59 @@ filename = os.path.join(dir, '../../config/rgb_options.ini')
 
 # Configuration for the matrix
 config = configparser.ConfigParser()
+config.read(filename)
+
+sysbus = dbus.SystemBus()
+systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
 
 # home route
 @app.route("/")
-def hello():
-    config.read(filename)
-    
+def saved_config():
     # Brightness from config file
     brightness = int(config['DEFAULT']['brightness'])
-    return render_template('index.html', brightness = brightness)
+    width = int(config['DEFAULT']['rows'])
+    height = int(config['DEFAULT']['columns'])
+    power = config['DEFAULT']['power']
+    return render_template('index.html', brightness = brightness, width = width, height = height, power = power)
 
-# serving form web page
-@app.route("/form")
-def form():
-    return render_template('form.html')
+# handling power status
+@app.route("/power", methods=["GET", "POST"])
+def handle_power():
+    power = request.form['power']
+    brightness = int(config['DEFAULT']['brightness'])
+    width = int(config['DEFAULT']['rows'])
+    height = int(config['DEFAULT']['columns'])
+    config.set('DEFAULT', 'power', request.form['power'])
+    if power == 'on':
+      job = manager.StartUnit('spotipi.service', 'replace')
+    else:
+      job = manager.StopUnit('spotipi.service', 'replace')
+    return render_template('index.html', brightness = brightness, width = width, height = height, power = power)
 
 # handling form data
 @app.route('/brightness', methods=['POST'])
-def handle_data():
+def handle_brightness():
     config.set('DEFAULT', 'brightness', request.form['brightness'])
-    with open(filename, 'w') as configfile:
+    width = int(config['DEFAULT']['rows'])
+    height = int(config['DEFAULT']['columns'])
+    power = config['DEFAULT']['power']
+    with open(filename, 'wb') as configfile:
         config.write(configfile)
-    return render_template('index.html', brightness = request.form['brightness'])
+    job = manager.RestartUnit('spotipi.service', 'fail')
+    return render_template('index.html', brightness = request.form['brightness'], width = width, height = height, power = power)
 
-app.run(debug = True) 
+# handling form data
+@app.route('/size', methods=['POST'])
+def handle_size():
+    config.set('DEFAULT', 'rows', request.form['width'])
+    config.set('DEFAULT', 'columns', request.form['height'])
+    brightness = int(config['DEFAULT']['brightness'])
+    power = config['DEFAULT']['power']
+    with open(filename, 'wb') as configfile:
+        config.write(configfile)
+    job = manager.RestartUnit('spotipi.service', 'fail')
+    return render_template('index.html', brightness = brightness, width = int(request.form['width']), height = int(request.form['height']), power = power)
+
+app.debug = True
+app.run(host='0.0.0.0', port=80) 
