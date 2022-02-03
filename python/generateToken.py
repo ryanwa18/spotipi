78aefd58f5e909d963602f546ba74a313cde90bd
Kyle Y <kyle@hive76.org>
2022-02-03 17:57:08 +0000

    Updates to getting an auth token and reducing bancdwidth requesting album images only on changes

--- python/generateToken.py
+++ python/generateToken.py
@@ -1,8 +1,12 @@
 import sys
 import spotipy
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
     scope = 'user-read-currently-playing'
-    token = util.prompt_for_user_token(username, scope)
+
+    # This way removes the need for a browser, it will instead give the URL to visit in the terminal
+    auth = SpotifyOAuth(scope=scope, open_browser=False)
+    token = auth.get_access_token()
